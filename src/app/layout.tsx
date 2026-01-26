75fbeb09f4bf6fc1dcb0b3279b7bdbc22c8f43bc
Shehryar <shehryar.dev@gmail.com>
2026-01-26 06:11:08 +0400

    Added seo metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,100 @@ export const metadata: Metadata = {
     title: "GREV",
     description:
         "Github Release Viewer - View and track releases of your favorite GitHub repositories with ease.",
+    keywords: [
+        "grev",
+        "github",
+        "release",
+        "tracker",
+        "statistic",
+        "viewer",
+        "download",
+        "count",
+    ],
+    openGraph: {
+        siteName: "GREV",
+        locale: "en_US",
+        url: "https://grev.shehryar.ae",
+        type: "website",
+        title: "GREV",
+        description:
+            "Github Release Viewer - View and track releases of your favorite GitHub repositories with ease.",
+        images: [
+            {
+                url: "https://user-images.githubusercontent.com/41040912/116804280-ad57f480-ab2e-11eb-9bb9-46e714c05ac3.png",
+                width: 800,
+                height: 300,
+                alt: "grev",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "GREV",
+        description:
+            "Github Release Viewer - View and track releases of your favorite GitHub repositories with ease.",
+        creator: "@devshehryar",
+        site: "@devshehryar",
+        images: [
+            {
+                url: "https://user-images.githubusercontent.com/41040912/116804280-ad57f480-ab2e-11eb-9bb9-46e714c05ac3.png",
+                width: 800,
+                height: 300,
+                alt: "grev",
+            },
+        ],
+    },
+    alternates: {
+        canonical: "https://grev.shehryar.ae",
+    },
+    metadataBase: new URL("https://grev.shehryar.ae"),
+    robots: {
+        index: true,
+        follow: true,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+        "max-video-preview": -1,
+        googleBot: "index, follow",
+    },
+    applicationName: "GREV",
+    appleWebApp: {
+        title: "GREV",
+        statusBarStyle: "default",
+        capable: true,
+    },
+    icons: {
+        icon: [
+            {
+                url: "/favicon.ico",
+                type: "image/x-icon",
+            },
+            // {
+            //     url: "/favicon-16x16.png",
+            //     sizes: "16x16",
+            //     type: "image/png",
+            // },
+            // add favicon-32x32.png, favicon-96x96.png, android-chrome-192x192.png
+        ],
+        shortcut: [
+            {
+                url: "/favicon.ico",
+                type: "image/x-icon",
+            },
+        ],
+        // apple: [
+        //     {
+        //         url: "/apple-icon-57x57.png",
+        //         sizes: "57x57",
+        //         type: "image/png",
+        //     },
+        //     {
+        //         url: "/apple-icon-60x60.png",
+        //         sizes: "60x60",
+        //         type: "image/png",
+        //     },
+        //     // add apple-icon-72x72.png, apple-icon-76x76.png, apple-icon-114x114.png, apple-icon-120x120.png, apple-icon-144x144.png, apple-icon-152x152.png, apple-icon-180x180.png
+        // ],
+    },
 };
 
 export default async function RootLayout({
